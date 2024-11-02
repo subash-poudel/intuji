57cadca32c7b5d6940a21d09d38a4267ccf4051c
Subash Poudel <blogsubash@gmail.com>
2024-11-02 13:10:31 +0545

    Fix get request for all requests

--- backend/src/services/eventService.ts
+++ backend/src/services/eventService.ts
@@ -2,7 +2,56 @@ import knex from "../db";
 import { EventRequestModel } from "../models/eventModels";
 
 export async function getAllEvents() {
-  return knex.raw(`SELECT * FROM events;`).then((res) => res.rows);
+  // return knex.raw(`SELECT * FROM events;`).then((res) => res.rows);
+  const events = await knex("events")
+    .join("participants", "events.id", "participants.event_id")
+    .select(
+      "events.id as event_id",
+      "events.title",
+      "events.description",
+      "events.duration",
+      "events.start_time",
+      "events.start_date",
+      "events.end_date",
+      "events.time_zone",
+      "events.location",
+      "events.recurrence",
+      "participants.id as participant_id",
+      "participants.name as participant_name",
+      "participants.email as participant_email",
+      "participants.rsvp_status"
+    );
+
+  const structuredResponse = events.reduce((acc, item) => {
+    let event = acc.find((e: any) => e.event_id === item.event_id);
+
+    if (!event) {
+      event = {
+        event_id: item.event_id,
+        title: item.title,
+        description: item.description,
+        duration: item.duration,
+        start_time: item.start_time,
+        start_date: item.start_date,
+        end_date: item.end_date,
+        time_zone: item.time_zone,
+        location: item.location,
+        recurrence: item.recurrence,
+        participants: [],
+      };
+      acc.push(event);
+    }
+    event.participants.push({
+      participant_id: item.participant_id,
+      name: item.participant_name,
+      email: item.participant_email,
+      rsvp_status: item.rsvp_status,
+    });
+
+    return acc;
+  }, []);
+
+  return structuredResponse;
 }
 
 export async function getAllExistingEventsForParticipants(
@@ -11,7 +60,6 @@ export async function getAllExistingEventsForParticipants(
   // TODO sql injection prevention. the sql is vunurable here
   const emails = participantEmails.map((e) => `'${e}'`).join(", ");
   const sql = `select * from events e where e.id in (select distinct p.event_id from participants p where p.email in (${emails}));`;
-  console.log("sql", sql);
   return knex.raw(sql).then((res) => res.rows);
 }
 
